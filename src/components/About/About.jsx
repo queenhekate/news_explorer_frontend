9a08a8562ea13986a00df6812ce6009ebe7d365f
Johanna Schnell <johannaschnell@gmail.com>
2025-02-10 21:17:56 +0000

    add About component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about">
+        <img></img>
+      <h2 className="about__heading">About the author</h2>
+      <p className="about__description">This block describes the project author. Here you should indicate your name, what you do, and which development technologies you know. You can also talk about your experience with TripleTen, what you learned there, and how you can help potential customers.</p>
+    </div>
+  );
+}
+
+export default About;
