14972767efee5cefe592db17ded6f2aef39a3145
Johanna Schnell <johannaschnell@gmail.com>
2025-02-10 00:50:10 +0000

    media queries for header

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,58 @@
+import React, { useState, useEffect } from "react";
 import './Navigation.css';
+import dropdownIcon from "../../assets/menu.png";
+import ReusableButton from "../ReuseableButton/ReusableButton";
 
 function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = (event) => {
+    setIsOpen(prevState => !prevState);
+    event.stopPropagation(); // Prevent event from propagating to the window click listener
+  };
+
+  const closeDropdown = (event) => {
+    // Close dropdown if user clicks outside of the dropdown
+    if (event.target.closest('.navigation') === null) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", closeDropdown);
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener("click", closeDropdown);
+    };
+  }, []);
+
   return (
-      <div className="navigation">
-        <button className="navigation__home">Home</button>
-        <button className="navigation__signin">Sign In</button>
+    <div className="navigation">
+      <div className="navigation__buttons">
+        <ReusableButton text="Home" className="navigation__home" />
+        <ReusableButton text="Sign In" className="navigation__signin" />
       </div>
+
+      {/* Dropdown Menu Button for Small Screens */}
+      <button className="navigation__dropdown" onClick={toggleDropdown}>
+        <img
+          src={dropdownIcon}
+          alt="Menu"
+          className="navigation__menu-image"
+        />
+      </button>
+
+      {/* Dropdown Menu Items */}
+      {isOpen && (
+        <div className="navigation__dropdown-content">
+          <div className="navigation__dropdown-container">
+          <ReusableButton text="Home" className="navigation__home" />
+          <ReusableButton text="Sign In" className="navigation__signin" />
+        </div>
+        </div>
+      )}
+    </div>
   );
 }
 
---
+++ src/components/ReuseableButton/ReusableButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReusableButton = ({ text, onClick, className }) => {
+  return (
+    <button className={className} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default ReusableButton;
