f880badca14f8694864fbee9c3df00c799961ad8
Johanna Schnell <johannaschnell@gmail.com>
2025-05-29 12:45:45 -0700

    new profile-photo

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import "./About.css";
-import profilePhoto from "../../assets/profile-photo.jpeg";
+import profilePhoto from "../../assets/profile-photo.jpg";
 
 function About() {
   return (
@@ -10,13 +10,15 @@ function About() {
       <div className="about__text">
         <h2 className="about__heading">About the author</h2>
         <p className="about__description">
-          Hello, I&apos;m Johanna Schnell, a web developer skilled in technologies
-          like JavaScript, React, Express, MongoDB, and Trello. I gained valuable
-          experience at TripleTen, where I worked on full-stack projects and
-          refined my problem-solving and development skills.<br />
-          <br />I&apos;m passionate about helping clients bring their ideas to life with clean, efficient
-          code and user-friendly web solutions. Let’s collaborate and build
-          something great!
+          Hello, I&apos;m Johanna Schnell, a web developer skilled in
+          technologies like JavaScript, React, Express, MongoDB, and Trello. I
+          gained valuable experience at TripleTen, where I worked on full-stack
+          projects and refined my problem-solving and development skills.
+          <br />
+          <br />
+          I&apos;m passionate about helping clients bring their ideas to life
+          with clean, efficient code and user-friendly web solutions. Let’s
+          collaborate and build something great!
         </p>
       </div>
     </div>
